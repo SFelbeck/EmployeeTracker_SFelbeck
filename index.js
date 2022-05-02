312426eaad03f306b55cd1dac825fb44000005a6
SFelbeck <stefanfelbeck@gmail.com>
2022-05-01 18:05:35 -0700

    add functions first pass

--- index.js
+++ index.js
@@ -61,16 +61,71 @@ const viewEmployees = () => {
     })
 }
 
-function addDepartment(){
-
+const addDepartment = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the new department?'
+        }
+    ])
+    db.query('INSERT INTO department (name) VALUES (?)', [res.name], (err, data) => {
+        console.log('Added new department!');
+        mainMenu();
+    })
 }
 
-function addRole(){
-
+const addRole = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the new role?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'In decimal form, what is the average salary of the new role?'
+        },
+        {
+            type: 'input',
+            name: 'department',
+            message: 'What department will this role be under?'
+        }
+    ])
+    db.query('INSERT INTO roles (name, salary, department_id) VALUES (?, ?, ?)', [res.name, res.salary, res.department], (err, data) => {
+        console.log('Added new role!');
+        mainMenu();
+    })
 }
 
-function addEmployee(){
-
+const addEmployee = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the first name of the new employee?'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the last name of the new employee?'
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What is the role of the new employee?'
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'What is the employee id of the manager for this employee?'
+        }
+    ])
+    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.role, res.manager], (err, data) => {
+        console.log('Added new employee!');
+        mainMenu();
+    })
 }
 
 function updateEmployee(){
